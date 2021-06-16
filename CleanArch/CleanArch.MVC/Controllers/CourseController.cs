923167627c70929d9962d2db20ed5d23d34cb866
Fernando Quiroga <fernando.quiroga@jbknowledge.com>
2021-06-16 12:41:20 -0300

    Commit presentation layer

---
+++ CleanArch/CleanArch.MVC/Controllers/CourseController.cs
@@ -0,0 +1,27 @@
+﻿using CleanArch.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArch.MVC.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _courseService.GetCources();
+            return View(model);
+        }
+    }
+}
